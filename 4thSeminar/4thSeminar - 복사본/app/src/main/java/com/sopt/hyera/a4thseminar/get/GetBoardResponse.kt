92e5c883f33584160b7a3bf60a071ff241abd37f
HyeranShin <hyebaragi__@naver.com>
2018-06-28 13:41:52 +0900

    Delete files

--- 4thSeminar/4thSeminar - 복사본/app/src/main/java/com/sopt/hyera/a4thseminar/get/GetBoardResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.sopt.hyera.a4thseminar.get
-
-data class GetBoardResponse (
-    var message : String,
-    val data : ArrayList<GetBoardResponseData>
-)
--- 4thSeminar/4thSeminar - 복사본/app/src/main/java/com/sopt/hyera/a4thseminar/post/PostBoardResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.sopt.hyera.a4thseminar.post
-
-class PostBoardResponse (
-    var message : String
-)
